913e483eb2b8fa939656545b7ac8ddc96130f5d0
VictorLVA <victorl@playtika.com>
2018-11-01 16:26:31 +0300

    practice

---
+++ Chapter2/Task9/Task9.java
@@ -0,0 +1,28 @@
+public class Task9 {
+    public static void main (String[] args) {
+        boolean a = false;
+        Boolean aa = new Boolean(true);
+        System.out.println(a + " " + aa);
+        char b = '-';
+        Character bb = new Character('+');
+        System.out.println(b + " " + bb);
+        byte by = 0;
+        byte byy = new Byte("1");
+        System.out.println(by + " " + byy);
+        short s = 2;
+        Short ss = new Short("3");
+        System.out.println(s + " " + ss);
+        int i = 4;
+        Integer ii = new Integer(5);
+        System.out.println(i + " " + ii);
+        long l = 6;
+        Long ll = new Long(7);
+        System.out.println(l + " " + ll);
+        float f = 8.8f;
+        Float ff = new Float(9.9);
+        System.out.println(f + " " + ff);
+        double d = 10.10;
+        Double dd = new Double(11.11);
+        System.out.println(d + " " + dd);
+    }
+}
